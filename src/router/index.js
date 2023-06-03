8924a9f8fd82a12cab9950ea2e742574434978ee
dt본체 <legavin1023@gmail.com>
2023-06-04 02:25:57 +0900

    히스토리->해시

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import { createRouter, createWebHistory } from "vue-router";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -16,8 +16,8 @@ const routes = [
   },
 ];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: "hash",
   routes,
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/nugbug/" : "/",
 });
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/nugbug/" : "/",
+};
+//npm run build
+//npm run deploy
